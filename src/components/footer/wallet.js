7dd62bd4731f43f54213578f7dee3c92ec92cb28
Harry Liu <2411mail@gmail.com>
2019-04-09 12:37:04 +0800

    footer

--- src/components/footer/wallet.js
+++ src/components/footer/wallet.js
@@ -1,37 +1,61 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Button } from 'antd'
 
-import './style.less'
+// import './style.less'
 
 import I18N from '../../I18N'
 
 
 export default class extends React.Component {
   render () {
-    const isEn = I18N.getLang() === 'en'
-    const appId = isEn ? <p className="sub-tag"></p> : <p className="sub-tag">(需海外Apple ID：<a href="/downloads/apple-id-tutorial.pdf">注册教程</a>)</p>
-    const iosImg = isEn ? 'wallet-ios@2x.jpg' : 'zh-wallet-ios@2x.jpg'
-    const androidImg = 'wallet-android@2x.jpg'
     return (
-      <nav className="wallet">
-        <div className="title">Elephant Wallet</div>
-        <ul>
-          <li>
-            <div className="group">
-              <div className="tag">{I18N.get('footer.wallet.ios')}</div>
-              {appId}
-            </div>
-            <img src={`../images/${iosImg}`} className="spacer" alt="wallet" />
-          </li>
-          <li>
-            <div className="group">
-              <div className="tag">{I18N.get('footer.wallet.android')}</div>
-              <p className="sub-tag"><a href="https://download.elastos.org/app/elephantwallet/elephant_wallet.apk" target="_blank" rel="noopener noreferrer">{I18N.get('footer.wallet.android.download')}</a></p>
-            </div>
-            <img src={`../images/${androidImg}`} className="spacer" alt="wallet" />
-          </li>
-          <li className="filler" />
-        </ul>
-      </nav>
+      <Container>
+        <Title>{I18N.get('footer.title')}</Title>
+        <div>{I18N.get('footer.store.title')}</div>
+        <List>
+          <ListItem>
+            <a href={I18N.get('footer.store.ios.storeLink')} target="_blank" rel="noopener noreferrer">
+              <img src={I18N.get('footer.store.ios.url')} className="spacer" alt="download url" />
+            </a>
+            <div>{I18N.get('footer.store.ios.help')}</div>
+            <Button>{I18N.get('footer.store.ios.helpLink')}</Button>
+          </ListItem>
+          <ListItem>
+            <a href={I18N.get('footer.store.android.storeLink')} target="_blank" rel="noopener noreferrer">
+              <img src={I18N.get('footer.store.android.url')} className="spacer" alt="download url" />
+            </a>
+            <div>{I18N.get('footer.store.android.help')}</div>
+            <Button>{I18N.get('footer.store.android.helpLink')}</Button>
+          </ListItem>
+        </List>
+
+        <div>{I18N.get('footer.qrCode.title')}</div>
+        <List>
+          <ListItem>
+            <div className="tag">{I18N.get('footer.qrCode.ios.title')}</div>
+            <img src={I18N.get('footer.qrCode.ios.url')} className="spacer" alt="wallet" />
+          </ListItem>
+          <ListItem>
+            <div className="tag">{I18N.get('footer.qrCode.android.title')}</div>
+            <img src={I18N.get('footer.qrCode.android.url')} className="spacer" alt="wallet" />
+          </ListItem>
+        </List>
+      </Container>
     )
   }
 }
+
+const Container = styled.div`
+`
+const Title = styled.div`
+  font-size: 32px;
+`
+const List = styled.ul`
+`
+const ListItem = styled.li`
+`
+const Link = styled.div`
+`
+const Text = styled.div`
+`
