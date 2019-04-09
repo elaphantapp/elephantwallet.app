3d30e31801bc65ee3d68b76b0dfa04501ef2fb5a
Harry Liu <2411mail@gmail.com>
2019-04-09 15:34:41 +0800

    added colors

--- src/constants/color.js
+++ src/constants/color.js
@@ -1,15 +1,17 @@
 export const bg = {
+  theme: '#d14258',
   dark: '#0F2D3B',
   lightGray: '#F1F5F8',
-  selected: '#45427e'
+  selected: '#45427e',
 }
 
 export const text = {
+  theme: '#d14258',
   white: '#fcfcfc',
   blue: '#4f789c',
   darkBlue: '#324256',
-  navy: '#0f2d3a'
+  navy: '#0f2d3a',
 }
 export const border = {
-  light: '#cfdae4'
+  light: '#cfdae4',
 }
