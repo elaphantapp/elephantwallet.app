4ef9cfc3983018e8dcd39827489d241115340f24
song sjun <song.sjun@hotmail.com>
2019-04-12 01:10:24 +0800

    Update zh.js

--- src/I18N/zh.js
+++ src/I18N/zh.js
@@ -28,23 +28,23 @@ export default {
 
   // hero section
   hero: {
-    welcome: 'WELCOME TO THE ELEPHANT WALLET',
-    title: 'A multi-currency wallet with DApp support',
-    btnText: 'Download the App Now',
-    help: '* Chinese users please click here for help with creating an Apple ID',
-    learnMore: 'LEARN MORE',
+    welcome: '欢迎来到大象钱包',
+    title: '支持多币种的DApp钱包',
+    btnText: '立即下载',
+    help: '* 申请海外Apple ID帮助',
+    learnMore: '了解更多',
   },
 
   // intro section
   intro: {
     about: {
-      title: 'About our app',
-      desc: 'Your personal digital property steward. You can manage your Bitcoin, Ethereum, ELA and many other digital coins; as well as the Elastos DID, which is based on the blockchain digital identity, allowing you to master your own data.',
+      title: '关于我们的App',
+      desc: '您的个人数字财产管家。 您可以管理您的比特币，以太坊，ELA和许多其他数字硬币; 以及基于区块链数字身份的Elastos DID，可让您掌握自己的数据。',
     },
     social: {
       telegram: {
-        title: 'Join Telegram',
-        desc: 'Discuss features and find answers to your questions in our telegram group. Everyone is welcome to join and come chat about the app.',
+        title: '加入 Telegram',
+        desc: '在我们的电报小组中讨论功能并找到问题的答案。 欢迎大家加入讨论。',
         btnText: 'Telegram',
         link: 'https://t.me/elastoswalletelephant',
       },
@@ -52,33 +52,33 @@ export default {
     feature: {
       feature_1: {
         title: 'Elastos DID',
-        desc: 'Supports Elastos DID, so you can fully grasp your identity on the internet as you master digital currency, and you can use the DID to determine your own data.',
+        desc: '支持Elastos DID，您不仅可以拥有您的数字货币，同时对您在互联网上的身份拥有自我主权，让您可以对自己的数据确权。',
       },
       feature_2: {
-        title: 'Multi-Currency Crypto Wallet',
-        desc: 'Supports BTC, ELA, ERC20 and other encrypted coins. No account, no login, just keep your 12 mnemonics safe and you control your property, no back-end server, no one collects your information.',
+        title: '多币种钱包',
+        desc: '支持 BTC, ELA, ERC20 和其它加密数字币. 不需要账户和登录, 只需要保存好12个助记词就可以掌握你的财产, 没有中心服务器, 没有人收集你的信息。',
       },
       feature_3: {
-        title: 'Open Source Project',
-        desc: 'Based on the open source bread wallet development; it has the same security features. Simple operation and page layout allow you to master the key to the blockchain world without having to learn too much.',
+        title: '开源项目',
+        desc: '基于开源的Bread钱包开发，具有与它相同的安全特性。 简单的操作和页面布局使您可以快速进入区块链世界，而无需学习太多东西。',
       },
     },
   },
 
   // footer section
   footer: {
-    title: 'DOWNLOAD THE APP TODAY',
+    title: '立即下载APP',
     store: {
-      title: 'Download on the App Store and Google Play Store',
+      title: '从 App Store 和 Google Play Store 下载',
       ios: {
-        help: 'Chinese users please use the button below for help with creating and Apple ID.',
-        btnText: 'Apple ID Help',
+        help: '需要海外Apple ID，请点击下面链接查看注册海外Apple ID的帮助。',
+        btnText: '注册海外Apple ID向导',
         helpLink: 'https://www.elastos.org/downloads/apple-id-tutorial.pdf',
         storeLink: 'https://itunes.apple.com/app/elastos-elephant-wallet/id1446241279',
         storeImg: images.appstore_button_img,
       },
       android: {
-        help: 'Use the button below to directly download the apk.',
+        help: '直接下载APK.',
         btnText: 'Android apk',
         helpLink: 'https://download.elastos.org/app/elephantwallet/elephant_wallet.apk',
         storeLink: 'https://play.google.com/store/apps/details?id=com.elastos.wallet',
@@ -86,13 +86,13 @@ export default {
       },
     },
     qrCode: {
-      title: 'You can also use the QR codes below:',
+      title: '你也可以直接扫码下载:',
       ios: {
-        title: 'iOS Version',
+        title: 'iOS版',
         url: images.wallet_android_img,
       },
       android: {
-        title: 'Android Version',
+        title: 'Android版',
         url: images.wallet_ios_img,
       },
     },
