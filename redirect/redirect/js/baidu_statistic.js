8f0fbaacbd07393d8083c9dd49e68301b7ac6838
Harry Liu <harry.liu@cyberrepublic.org>
2019-05-22 15:06:42 +0800

    Updates

---
+++ redirect/redirect/js/baidu_statistic.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?9be1f85c861a6f857796dce15b710493";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
